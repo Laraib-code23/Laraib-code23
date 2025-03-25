d3539d21eee5999e40d05665afc51c39fc5fee96
Laraib-code23 <aspirationcs71@gmail.com>
2025-03-25 13:46:22 +0500

    Add files via upload
    Operators are special symbols or keywords that perform operations on values and variables it could be of 7 types
    1.Arithmetic(+,-,*,%,//,/,**)
    2.Comparision(==,!=, >,<,<=,>= )
    3.Logical(and , or , not)
    4.Bitwise(&,',<<,>>,-,^)
    5.Assignment (=,+=,-=,*=,/=,//=,%=,**=, . . . . . .)
    6.Identify (is , is not)
    7.Membership(in, in not)

---
+++ Operator.py
@@ -0,0 +1,14 @@
+a=25
+b=4
+print(a+b) #Add
+print(a-b)# Difference
+print(a*b)# Product
+print(a//b)# Floor divison
+# finding Remainder
+x=50
+y=7
+print(x%y)# remainder
+# exponent
+x=2
+y=5
+print(x**y)
