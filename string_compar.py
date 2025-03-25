46e7b5f9f09d20494be155588b23a4c751d88070
Laraib-code23 <aspirationcs71@gmail.com>
2025-03-25 14:14:13 +0500

    Add files via upload

---
+++ string_compar.py
@@ -0,0 +1,6 @@
+x=str(input("Enter a string:"))
+y=str(input("Enter another string:"))
+if len(x)==len(y):
+ print("Both lengths are equal:")
+else:
+ print("neither the length nor the strings are same:") 
