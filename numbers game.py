377fde5ec7a06134dd399d68fb4042099f6bc282
Laraib-code23 <aspirationcs71@gmail.com>
2025-03-25 14:08:39 +0500

    Add files via upload

---
+++ numbers game.py
@@ -0,0 +1,9 @@
+x=int(input("Enter a number: "))
+if x==100:
+    print("x is equal to 100:")
+elif x>100:
+    print("x is greater than to 100:")
+elif x<100:
+    print("x is lessen to 100:")
+else:
+    ("Are you sure it is a number")
