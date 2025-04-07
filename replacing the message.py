bedbee953dd7179966b9e4681fb71d9580a5e26d
Laraib-code23 <aspirationcs71@gmail.com>
2025-04-07 22:39:54 +0500

    Add files via upload
    here we learn about replacing the string by using "replace() " in this program we replace all the occurrences of "bad" into "good"

---
+++ replacing the message.py
@@ -0,0 +1,3 @@
+boy='''This is a joe .he is a bad boy in our all street.He helps everyone and 
+ everyone love his friendly and pleasure behaviour.He always sit with street bad boys .'''
+print(boy.replace("bad","good"))
