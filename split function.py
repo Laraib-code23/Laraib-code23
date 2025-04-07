8233d91efe83324f376e8c29e49a9c80155aec84
Laraib-code23 <aspirationcs71@gmail.com>
2025-04-07 22:45:00 +0500

    Add files via upload

---
+++ split function.py
@@ -0,0 +1,7 @@
+sentence='''This is a joe .he is a bad boy in our all street.He helps everyone and 
+ everyone love his friendly and pleasure behaviour.He always sit with street bad boys .'''
+sentance=sentence.split()
+first_word=sentance[0]
+last_word=sentance[-1]
+print("first word of provided message is:",first_word)
+print("last word of provided message is:",last_word)
