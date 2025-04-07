0e3fa4f89779a39305d18f6cdc342ac9617d6b07
Laraib-code23 <aspirationcs71@gmail.com>
2025-04-07 23:27:43 +0500

    Add files via upload

---
+++ welcome function.py
@@ -0,0 +1,7 @@
+#def welcom():
+  #  print("welcome to the python programming:")
+#welcom()
+#if we call by name we can pass a parameter like
+def welcom(name):
+  print("welcome dear",name)
+welcom("alia")
