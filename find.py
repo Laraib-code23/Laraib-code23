82f36a8f5cf3a3c2e1185d66cf144dbe30eeb547
Laraib-code23 <aspirationcs71@gmail.com>
2025-04-07 22:45:35 +0500

    Add files via upload

---
+++ find.py
@@ -0,0 +1,5 @@
+sentance=str(input('''enter a message'''))
+if sentance.find("python") != -1:
+   print("Yes provided sentance contains a word python")
+
+print("this sentance not talk about python")
