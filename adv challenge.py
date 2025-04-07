1d5017ebdeb9e7eb8f4e40575eec3f55f93fbb26
Laraib-code23 <aspirationcs71@gmail.com>
2025-04-07 22:54:28 +0500

    adv challenge.py

--- adv challenge.py
+++ adv challenge.py
@@ -1,3 +1,4 @@
+#write a script takes a string from user and reversed that string then checked if the string is palindrome or not:
 word=str(input("enter a string:"))
 reversed_word=word[::-1]
 if word==reversed_word:
