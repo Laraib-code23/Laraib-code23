de0bbfbd143622ba3d87d1a611c0edcb05e99d85
Laraib-code23 <aspirationcs71@gmail.com>
2025-04-09 18:39:28 +0500

    Add files via upload

---
+++ check.py
@@ -0,0 +1,15 @@
+#check even or odd
+#def number(x):
+ #if x % 2==0:
+    #print(" the provided number is even:")
+ #else:
+   # print("it is an odd number")
+#number(9)
+#2nd methode here program takes input from user;
+def number(x):
+ if x % 2==0:
+    print(" the provided number is even:")
+ else:
+    print("it is an odd number")
+num=int(input("Enter a number: "))
+number(num)
